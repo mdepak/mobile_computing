3e9918b53abe4a1c22dd262a40fbd097c6ef682e
mdepak <m.deepak@ymail.com>
2018-03-07 18:19:57 -0700

    assignment 1

---
+++ assign_1 3/README.txt
@@ -0,0 +1,37 @@
+Group 2 - Assignment 1
+
+
+
+Android app for entering the patient details and showing the random data generated when the RUN button is clicked.
+
+The app used the GraphView provided for the assignment and does not use any other third party components.
+
+Functionality:
+When the run button is clicked, Update thread is started and the thread updates the view by passing random generated values to the view.
+
+The thread sleeps for 1000 milliseconds and then updates the view in regular intervals untill the stop button is clicked.
+
+When the stop button is clicked we terminate the loop of the thread by modifying the signal variable.
+
+Youtube link - https://youtu.be/wdhGlRnO0eE
+
+
+API Level 21
+
+Group 2
+Vinoth Vijayakumar
+1213218147
+vvijay22@asu.edu
+
+
+Deepak Mahudeswaran
+1213175078
+dmahudes@asu.edu
+
+Naga Subrahmanya Sai Tarun Malladi
+1213169605
+nmalladi@asu.edu
+
+Hariharan Adhithya Venkatraman
+1213221267
+hvenkat4@asu.edu
---
+++ assign_1 3/app/src/main/java/edu/asu/cidse/mc/group2/MainActivityFragment.java
@@ -0,0 +1,161 @@
+package edu.asu.cidse.mc.group2;
+
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    // View for the graph showing data
+
+    private GraphView graphView;
+    UpdateThread updateThread;
+    Queue<Float> valList;
+
+    // Horizontal axis values
+    String[] horAxis= {"2700","2750","2800","2850","2900","2950","3000","3150","3200"};
+
+    // Vertical axis values
+    String[] verAxis = {"500", "1000", "1500","2000"};
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        float[] values = new float[10];
+        valList = new LinkedList();
+
+        // Initialize the values to be zero
+        for (int i = 0; i < 10; i++) {
+            float val = new Random().nextFloat();
+            val = 0;
+            values[i] = val;
+            valList.add(val);
+        }
+
+        RelativeLayout rootLayout = rootView.findViewById(R.id.rel_layout);
+
+        // Initialize view in the onCreate and add to the layout
+        graphView = new GraphView(getContext(), values, "ECG readings",horAxis , verAxis, true);
+        updateThread = new UpdateThread(graphView, valList);
+        graphView.setBackgroundColor(Color.BLACK);
+        graphView.setVisibility(View.INVISIBLE);
+
+        rootLayout.addView(graphView);
+
+
+        Button runButton = rootView.findViewById(R.id.runBtn);
+        Button stopButton = rootView.findViewById(R.id.stopBtn);
+
+        // OnClickListener for the run button click
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /**
+             *  Handles the run button click
+             *  Starts the thread if its not alive and the thread updates the view with random generated data
+             *
+             */
+            public void onClick(View view) {
+                if (updateThread == null || !updateThread.isAlive()) {
+                    updateThread =new UpdateThread(graphView, valList);
+                    updateThread.start();
+                    graphView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        // OnClickListener for the Stop button click
+        stopButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            /**
+             *  Handles the stop button click
+             *
+             *  Stops the thread by signalling and clears the view
+             */
+            public void onClick(View view) {
+                if (updateThread != null) {
+                    valList = updateThread.getValues();
+
+                    updateThread.signalStop();
+                    updateThread = null;
+                    graphView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        return rootView;
+    }
+
+
+    /**
+     * Thread for updating the random values in the GraphView
+     */
+    class UpdateThread extends Thread {
+
+        private Queue<Float> values; // Values that the view displays
+        private GraphView graphView; // View to be updated
+        private Random random;
+        boolean execute; // Signal value for the termination of thread
+
+        public UpdateThread(GraphView graphView, Queue<Float> values) {
+            this.graphView = graphView;
+            this.values = values;
+            random = new Random();
+
+        }
+
+        // Returns the state of the values generated so far.
+        public Queue<Float> getValues() {
+            return values;
+        }
+
+        // Modify the signal variable so that the thread completes execution
+        public void signalStop() {
+            execute = false;
+        }
+
+        @Override
+        public void run() {
+            execute = true;
+            while (execute) {
+
+                values.add(random.nextFloat());
+                values.remove();
+
+                // In the values, maintain atmost 10 values and ane in each iteration add one
+                // random values to the queue and remove one element from the queue
+
+                float[] val = new float[values.size()];
+                int i = 0;
+                for (Float value : values)
+                    val[i++] = value.floatValue();
+
+                graphView.setValues(val);
+                // Invalidate the view to render again
+                graphView.postInvalidate();
+                try {
+                    // Sleep between different updates of the view
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
