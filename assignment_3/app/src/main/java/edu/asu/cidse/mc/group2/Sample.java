3945da583b739e8f0b0b8cb57d570366a6ef2cf2
mdepak <m.deepak@ymail.com>
2018-04-05 20:09:19 -0700

    Training sample

---
+++ assignment_3/app/src/main/java/edu/asu/cidse/mc/group2/Sample.java
@@ -0,0 +1,13 @@
+package edu.asu.cidse.mc.group2;
+
+import java.util.List;
+
+/**
+ * Created by student on 4/5/18.
+ */
+
+public class Sample {
+    int id;
+    List<AccSample> accSampleList;
+    int label;
+}
