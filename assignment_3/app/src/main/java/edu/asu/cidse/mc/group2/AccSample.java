88bc297ac00629dc8f9f96114348eb67f867ebb2
HariharanAdhithya <hvenkat4@asu.edu>
2018-04-05 18:19:40 -0700

    AccSample class

---
+++ assignment_3/app/src/main/java/edu/asu/cidse/mc/group2/AccSample.java
@@ -0,0 +1,45 @@
+package edu.asu.cidse.mc.group2;
+
+/**
+ * Created by aSh2277 on 4/5/18.
+ */
+
+public class AccSample {
+
+    public AccSample(float accx, float accy, float accz) {
+        this.accx = accx;
+        this.accy = accy;
+        this.accz = accz;
+    }
+
+    float accx;
+    float accy;
+
+    public float getAccx() {
+        return accx;
+    }
+
+    public void setAccx(float accx) {
+        this.accx = accx;
+    }
+
+    public float getAccy() {
+        return accy;
+    }
+
+    public void setAccy(float accy) {
+        this.accy = accy;
+    }
+
+    public float getAccz() {
+        return accz;
+    }
+
+    public void setAccz(float accz) {
+        this.accz = accz;
+    }
+
+    float accz;
+
+
+}
