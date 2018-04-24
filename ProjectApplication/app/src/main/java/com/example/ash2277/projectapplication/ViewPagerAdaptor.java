4c00f7914e3103f50904a1e1d74146a0b4c491c5
HariharanAdhithya <hvenkat4@asu.edu>
2018-04-24 00:42:08 -0700

    Left with segmentation

---
+++ ProjectApplication/app/src/main/java/com/example/ash2277/projectapplication/ViewPagerAdaptor.java
@@ -0,0 +1,57 @@
+package com.example.ash2277.projectapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+public class ViewPagerAdaptor extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private List<Uri> images;
+
+    public ViewPagerAdaptor(Context context, List<Uri> images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slider, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imgView);
+        imageView.setImageURI(images.get(position));
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+
+    }
+}
